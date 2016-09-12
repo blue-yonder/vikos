7c4e74db6d3a1719e2d11828a23ed2bde644fb9f
Markus Klein <markus-klein@live.de>
2016-09-13 00:11:57 +0200

    made mean.rs more concise

--- examples/mean.rs
+++ examples/mean.rs
@@ -1,24 +1,19 @@
 extern crate vikos;
-use vikos::{model, cost, teacher, learn_history, Model};
+use vikos::{model, cost, teacher, learn_history};
 
 fn main(){
 
     //mean is 9, but of course we do not know that yet
-    let history = [
-    (2.0, 1.0), (3.0, 3.0), (3.5, 4.0),
-    (5.0, 7.0), (5.5, 8.0), (7.0, 11.0),
-    (16.0, 29.0)
-    ];
+    let history = [1.0, 3.0, 4.0, 7.0, 8.0, 11.0, 29.0];
     // The mean is just a simple number ...
     let mut model = model::Constant::new(0.0);
     // ... which minimizes the square error
     let cost = cost::LeastSquares{};
     // Use stochasic gradient descent with an annealed learning rate
     let teacher = teacher::GradientDescentAl{ l0 : 0.3, t : 4.0 };
-    // Train 100 (admittedly repetitive) events
-    learn_history(&teacher, &cost, &mut model, history.iter().cycle().take(100).cloned());
-    // We need an input vector for predictions, the 42 will not influence the mean
-    println!("{}", model.predict(&42.0));
+    // Train 100 (admitettly repetitive) events
+    // We use the `map` iterator adaptor to extend an empty feature vector to each data point
+    learn_history(&teacher, &cost, &mut model, history.iter().cycle().take(100).map(|&y|((),y)));
     // Since we know the model's type is `Constant`, we could just access the members
     println!("{}", model.c);
 }
